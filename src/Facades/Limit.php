0ac3aac3413538910b46487dadf5939d98bce7d4
williamcruzme <sv.johncruz@gmail.com>
2022-06-10 17:53:50 -0600

    🎨 Formatting

--- src/Facades/Limit.php
+++ src/Facades/Limit.php
@@ -4,20 +4,47 @@ namespace Williamcruzme\Limits\Facades;
 
 use Illuminate\Support\Facades\Facade;
 use Williamcruzme\Limits\Factory;
+use Williamcruzme\Limits\Repository;
 
 /**
- * @method static void register($key, $class)
- * @method static array limits()
- * @method static mixed resolve(\Williamcruzme\Limits\Repository $repository)
- * @method static void defaultLimit(callable $defaultLimitCallback)
- * @method static mixed resolveDefaultLimit($limit, $key)
  * @method static self actingAs(\Illuminate\Contracts\Auth\Authenticatable $user)
  * @method static mixed user()
  */
 class Limit extends Facade
 {
+    public static $limits = [];
+
+    public static $defaultLimitCallback;
+
     protected static function getFacadeAccessor()
     {
         return Factory::class;
+    }
+
+    public static function register($key, $class)
+    {
+        static::$limits[$key] = $class;
+    }
+
+    public static function limits()
+    {
+        return static::$limits;
+    }
+
+    public static function resolve(Repository $repository)
+    {
+        return new static::$limits[$repository->name];
+    }
+
+    public static function defaultLimit(callable $defaultLimitCallback)
+    {
+        static::$defaultLimitCallback = $defaultLimitCallback;
+    }
+
+    public static function resolveDefaultLimit($limit, $key)
+    {
+        if (static::$defaultLimitCallback) {
+            return call_user_func(static::$defaultLimitCallback, $limit, $key);
+        }
     }
 }
--- src/Factory.php
+++ src/Factory.php
@@ -3,42 +3,17 @@
 namespace Williamcruzme\Limits;
 
 use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Str;
 
 class Factory
 {
-    public static $limits = [];
-
-    public static $defaultLimitCallback;
-
     protected $user;
 
-    public static function register($key, $class)
+    public function __construct()
     {
-        static::$limits[$key] = $class;
-    }
-
-    public static function limits()
-    {
-        return static::$limits;
-    }
-
-    public static function resolve(Repository $repository)
-    {
-        return new static::$limits[$repository->name];
-    }
-
-    public static function defaultLimit(callable $defaultLimitCallback)
-    {
-        static::$defaultLimitCallback = $defaultLimitCallback;
-    }
-
-    public static function resolveDefaultLimit($limit, $key)
-    {
-        if (static::$defaultLimitCallback) {
-            return call_user_func(static::$defaultLimitCallback, $limit, $key);
-        }
+        $this->user = Auth::guard('sanctum')->user();
     }
 
     public function actingAs(Authenticatable $user)
